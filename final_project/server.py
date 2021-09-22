f97e833f162d0e065844ee21f83315319eaa0317
Alagaraja Ravindra Raja <mcaraju@hotmail.com>
2021-09-22 17:31:52 +0530

    Implementation of ServerPy

--- final_project/server.py
+++ final_project/server.py
@@ -1,7 +1,7 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-import translator from machinetranslation
+
 
 app = Flask("Web Translator")
 
@@ -19,7 +19,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
